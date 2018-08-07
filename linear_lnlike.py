bb99e65b8eeb53b42aeec9e59b346535a7c7e4f2
Ryan Cloutier <cloutier@astro.utoronto.ca>
2018-08-07 16:26:16 -0400

    updated confirm transit coefficients and about to test

--- linear_lnlike.py
+++ linear_lnlike.py
@@ -5,7 +5,8 @@ import rvs, mcmc1, batman
 from scipy.interpolate import LinearNDInterpolator as lint
 
 global dispersion_sig, depth_sig, bimodalfrac
-dispersion_sig, depth_sig, bimodalfrac = 3., 3., .5
+#dispersion_sig, depth_sig, bimodalfrac = 3., 3., .5
+dispersion_sig, depth_sig, bimodalfrac = 3., 2.45, .5
 
 def lnlike(bjd, f, ef, fmodel):
     return -.5*(np.sum((f-fmodel)**2 / ef**2 - np.log(1./ef**2)))
