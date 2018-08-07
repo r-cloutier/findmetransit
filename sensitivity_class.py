56c9d5f4a104b87a92d5e11913676eb22921b32d
Ryan Cloutier <cloutier@astro.utoronto.ca>
2018-08-07 12:07:03 -0400

    added plotting function to visualize FPs in the sensitivity calculation results

--- sensitivity_class.py
+++ sensitivity_class.py
@@ -146,6 +146,79 @@ class Sensitivity_grid:
             plt.show()
         plt.close('all')
 
+
+    def plot_FP_correction(self, Pgrid=True, pltt=True, label=False):
+        if Pgrid:
+	    xarr, xgrid = self.Ps, self.Pgrid
+	    xFP, xlabel = self.PsFP, 'Period [days]'
+        else:
+            xarr, xgrid = self.Fs, self.Fgrid
+	    xFP, xlabel = self.FsFP, 'Insolation [S$_{\oplus}$]'
+
+        # plot planet detections
+        fig = plt.figure(figsize=(12,4))
+        ax1 = fig.add_subplot(131)
+        N_det, x, y = np.histogram2d(xarr[self.detected==1], self.rps[self.detected==1],
+                                     bins=(xgrid, self.rpgrid))
+        cax1 = ax1.pcolormesh(x, y, N_det.T, cmap=truncate_colormap(plt.get_cmap('rainbow'),0,1))
+        cbar_axes1 = fig.add_axes([.07,.1,.27,.04])
+        cbar1 = fig.colorbar(cax1, cax=cbar_axes1, orientation='horizontal')
+        cbar1.set_label('Number of detected (simulated) planets', labelpad=.1)
+        if Pgrid:
+            ax1.axvline(self.Pgrid.max()/2, ls='--', c='k')
+            ax1.axvline(self.Pgrid.max()/3, ls='--', c='k')
+            ax1.set_xlim((self.Pgrid.min(),self.Pgrid.max()))
+        else:
+	    ax1.set_xlim((self.Fgrid.max(),self.Fgrid.min()))
+        ax1.set_ylim((self.rpgrid.min(),self.rpgrid.max()))
+        ax1.set_xscale('log'), ax1.set_yscale('log')
+        ax1.set_xlabel(xlabel), ax1.set_ylabel('r$_p$ [R$_{\oplus}$]')
+
+        # plot planet false positives
+        ax2 = fig.add_subplot(132)
+        N_FP, x, y = np.histogram2d(xFP, self.rpsFP, bins=(xgrid, self.rpgrid))
+        cax2 = ax2.pcolormesh(x, y, np.log10(N_FP).T, cmap=truncate_colormap(plt.get_cmap('rainbow'),0,1))
+        cbar_axes2 = fig.add_axes([.39,.1,.27,.04])
+        cbar2 = fig.colorbar(cax2, cax=cbar_axes2, orientation='horizontal')
+        cbar2.set_label('log$_{10}$ Number of false positives', labelpad=.1)
+        if Pgrid:
+            ax2.axvline(self.Pgrid.max()/2, ls='--', c='k')
+            ax2.axvline(self.Pgrid.max()/3, ls='--', c='k')
+            ax2.set_xlim((self.Pgrid.min(),self.Pgrid.max()))
+        else:
+	    ax2.set_xlim((self.Fgrid.max(),self.Fgrid.min()))
+        ax2.set_ylim((self.rpgrid.min(),self.rpgrid.max()))
+        ax2.set_xscale('log'), ax2.set_yscale('log'), ax2.set_xlabel(xlabel)
+
+        # plot yield correction (i.e. reduce the yield because of the fraction of FPs): yield(P,rp) *= 1 - N_FP(P,rp) / (N_detected(P,rp) + N_FP(P,rp))
+        ax3 = fig.add_subplot(133)
+        yield_correction = 1 - N_FP / (N_det + N_FP)
+        yield_correction[N_det+N_FP==0] = 1.
+        cax3 = ax3.pcolormesh(xgrid, self.rpgrid, yield_correction.T,
+                              cmap=truncate_colormap(plt.get_cmap('rainbow'),0,1))
+        cbar_axes3 = fig.add_axes([.71,.1,.27,.04])
+        cbar3 = fig.colorbar(cax3, cax=cbar_axes3, orientation='horizontal')
+        cbar3.set_label('Multiplicative yield correction', labelpad=.1)
+        if Pgrid:
+            ax3.axvline(self.Pgrid.max()/2, ls='--', c='k')
+            ax3.axvline(self.Pgrid.max()/3, ls='--', c='k')
+            ax3.set_xlim((self.Pgrid.min(),self.Pgrid.max()))
+        else:
+	    ax3.set_xlim((self.Fgrid.max(),self.Fgrid.min()))
+        ax3.set_ylim((self.rpgrid.min(),self.rpgrid.max()))
+        ax3.set_xscale('log'), ax3.set_yscale('log'), ax3.set_xlabel(xlabel)
+    
+        fig.subplots_adjust(bottom=.29, left=.06, right=.99, top=.97, wspace=.12)
+        if label:
+	    try:
+	        os.mkdir('plots')
+	    except OSError:
+	        pass
+            plt.savefig('plots/FPgrid_%s.png'%self.prefix)
+        if pltt:
+            plt.show()
+        plt.close('all')
+    
         
     def pickleobject(self):
         fObj = open('%s_SensitivityGrid'%self.fname_full, 'wb')
